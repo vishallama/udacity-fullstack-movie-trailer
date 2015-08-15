26eba1f16c44ed6693b2a575a6a2c5ebef9401b5
Vishal Lama <contact@vishallama.org>
2015-08-15 12:23:47 -0600

    Create Movie object city_lights

---
+++ entertainment_center.py
@@ -0,0 +1,33 @@
+# entertainment_center.py
+import media
+
+__author__ = 'vishal lama'
+
+city_lights = media.Movie(
+    "City Lights",
+    "A tramp falls in love with a beautiful blind girl. Her family is in "
+    "financial trouble. The tramp's on-and-off friendship with a wealthy "
+    "man allows him to be the girl's benefactor and suitor.",
+    "https://upload.wikimedia.org/wikipedia/en/f/f2/City_Lights_film.jpg",
+    "https://www.youtube.com/watch?v=b2NTUnujk1I",
+    ["Charles Chaplin", "Virginia Cherrill",
+     "Florence Lee", "Harry Myers",
+     "Al Ernest Garcia", "Hank Mann"
+    ],
+    "7 March 1931",
+    "G",
+    "English",
+    "87 min",
+    "Charles Chaplin Productions",
+    ["When the film opened on 31 January 1931, Albert Einstein joined "
+     "Charles Chaplin at the theater. When the film opened in England, "
+     "George Bernard Shaw joined him.",
+     "Charles Chaplin's personal favorite of all his films.",
+     "Russian director Andrei Tarkovsky cited this as his favorite film. "
+     "Woody Allen also calls it 'Chaplin's best picture'.",
+     "In 2008, this film was voted #1 on AFI's list over the ten best "
+     "romantic comedies of all time.",
+     "Virginia Cherrill was cast on a whim when Charles Chaplin spotted "
+     "her at a boxing match."
+     ]
+   )
