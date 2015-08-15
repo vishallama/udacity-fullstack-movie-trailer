ed5bcc65faaf12b4707226aad2899b43bdbee27b
Vishal Lama <contact@vishallama.org>
2015-08-15 12:24:28 -0600

    Write tests for Movie object city_lights

---
+++ test.py
@@ -0,0 +1,18 @@
+# test.py
+import entertainment_center
+
+__author__ = 'vishal'
+
+# Testing movie: City Lights
+print(entertainment_center.city_lights.title)
+print(entertainment_center.city_lights.storyline)
+print(entertainment_center.city_lights.poster_image_url)
+print(entertainment_center.city_lights.trailer_youtube_url)
+print(entertainment_center.city_lights.lead_actors)
+print(entertainment_center.city_lights.release_date)
+print(entertainment_center.city_lights.mpaa_rating)
+print(entertainment_center.city_lights.language)
+print(entertainment_center.city_lights.runtime)
+print(entertainment_center.city_lights.production_company)
+print(entertainment_center.city_lights.trivia)
+
