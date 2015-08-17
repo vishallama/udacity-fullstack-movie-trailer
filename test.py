2c8dcc578c0997dcd1339ca496b9c36387b0efdf
Vishal Lama <contact@vishallama.org>
2015-08-17 13:58:51 -0600

    Runs tests for Movie 'Forrest Gump'

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # test.py
 import unittest
 from entertainment_center \
-    import city_lights, shawshank_redemption, godfather
+    import city_lights, shawshank_redemption, godfather, \
+        forrest_gump
 
 
 # Test: City Lights
@@ -21,10 +22,10 @@ class MovieTestCityLights(unittest.TestCase):
             "https://www.youtube.com/watch?v=b2NTUnujk1I",
             city_lights.trailer_youtube_url)
 
-    def test_lead_actors_charles_chaplin(self):
+    def test_lead_actors_1(self):
         self.assertEqual("Charles Chaplin", city_lights.lead_actors[0])
 
-    def test_lead_actors_hank_mann(self):
+    def test_lead_actors_1(self):
         self.assertEqual("Hank Mann", city_lights.lead_actors[-1])
 
     def test_release_date(self):
@@ -53,10 +54,10 @@ class MovieTestShawshankRedemption(unittest.TestCase):
             "https://www.youtube.com/watch?v=6hB3S9bIaco",
             shawshank_redemption.trailer_youtube_url)
 
-    def test_lead_actors_charles_chaplin(self):
+    def test_lead_actors_1(self):
         self.assertEqual("Tim Robbins", shawshank_redemption.lead_actors[0])
 
-    def test_lead_actors_hank_mann(self):
+    def test_lead_actors_2(self):
         self.assertEqual("James Whitmore", shawshank_redemption.lead_actors[-1])
 
     def test_release_date(self):
@@ -85,10 +86,10 @@ class MovieTestGodfather(unittest.TestCase):
             "https://www.youtube.com/watch?v=sY1S34973zA",
             godfather.trailer_youtube_url)
 
-    def test_lead_actors_charles_chaplin(self):
+    def test_lead_actors_1(self):
         self.assertEqual("Marlon Brando", godfather.lead_actors[0])
 
-    def test_lead_actors_hank_mann(self):
+    def test_lead_actors_2(self):
         self.assertEqual("Diane Keaton", godfather.lead_actors[-1])
 
     def test_release_date(self):
@@ -98,6 +99,39 @@ class MovieTestGodfather(unittest.TestCase):
         self.assertEqual(
             "According to Francis Ford Coppola, the film took 62 days to shoot.",
             godfather.trivia[-1])
+
+
+# Test: Forrest Gump
+class MovieTestForrestGump(unittest.TestCase):
+
+    def test_title(self):
+        self.assertEqual(
+            "Forrest Gump",forrest_gump.title)
+
+    def test_poster_image_url(self):
+        self.assertEqual(
+            "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+            forrest_gump.poster_image_url)
+
+    def test_trailer_youtube_url(self):
+        self.assertEqual(
+            "https://www.youtube.com/watch?v=bLvqoHBptjg",
+            forrest_gump.trailer_youtube_url)
+
+    def test_lead_actors_1(self):
+        self.assertEqual("Tom Hanks", forrest_gump.lead_actors[0])
+
+    def test_lead_actors_2(self):
+        self.assertEqual("Rebecca Williams", forrest_gump.lead_actors[-1])
+
+    def test_release_date(self):
+        self.assertEqual("6 July 1994", forrest_gump.release_date)
+
+    def test_trivia(self):
+        self.assertEqual(
+            "Gary Sinise's lower legs were wrapped in a special blue fabric that "
+            "allowed them to be digitally removed later.",
+            forrest_gump.trivia[3])
 
 
 if __name__ == '__main__':
