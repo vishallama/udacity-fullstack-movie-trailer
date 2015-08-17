960518af0c345969d6a2cc44f9df998e752e7ee3
Vishal Lama <contact@vishallama.org>
2015-08-17 12:58:37 -0600

    Add movie 'The Godfather'

--- entertainment_center.py
+++ entertainment_center.py
@@ -14,7 +14,7 @@ city_lights = media.Movie(
     "G",
     "English",
     "87 min",
-    "Charles Chaplin Productions",
+    ["Charles Chaplin Productions"],
     ["When the film opened on 31 January 1931, Albert Einstein joined "
      "Charles Chaplin at the theater. When the film opened in England, "
      "George Bernard Shaw joined him.",
@@ -24,13 +24,16 @@ city_lights = media.Movie(
      "In 2008, this film was voted #1 on AFI's list over the ten best "
      "romantic comedies of all time.",
      "Virginia Cherrill was cast on a whim when Charles Chaplin spotted "
-     "her at a boxing match." ]
-   )
+     "her at a boxing match."]
+)
 
 shawshank_redemption = media.Movie(
     "The Shawshank Redemption",
-    "Two imprisoned men bond over a number of years, finding solace and "
-    "eventual redemption through acts of common decency.",
+    "Andy Dufresne is a young and successful banker whose life changes "
+    "drastically when he is convicted and sentenced to life imprisonment "
+    "for the murder of his wife and her lover. Set in the 1940s, the "
+    "film shows how Andy, with the help of his friend Red, the prison "
+    "entrepreneur, turns out to be a most unconventional prisoner. ",
     "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
     "https://www.youtube.com/watch?v=6hB3S9bIaco",
     ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler",
@@ -39,7 +42,7 @@ shawshank_redemption = media.Movie(
     "R",
     "English",
     "142 min",
-    "Castle Rock Entertainment",
+    ["Castle Rock Entertainment"],
     ["Morgan Freeman's favorite film of his own.",
      "Although a very modest hit in theaters, it became one of the highest "
      "grossing video rentals of all time.",
@@ -51,5 +54,33 @@ shawshank_redemption = media.Movie(
      "(1999).",
      "To prepare for his role as Andy Dufresne, Tim Robbins actually spent "
      "some time in solitary confinement."]
+)
 
+godfather = media.Movie(
+    "The Godfather",
+    "When the aging head of a famous crime family decides to transfer his "
+    "position to one of his subalterns, a series of unfortunate events start "
+    "happening to the family, and a war begins between all the well-known "
+    "families leading to insolence, deportation, murder and revenge, and "
+    "ends with the favorable successor being finally chosen.",
+    "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
+    "https://www.youtube.com/watch?v=sY1S34973zA",
+    ["Marlon Brando", "Al Pacino", "James Caan", "Richard S. Castellano",
+     "Robert Duvall", "Sterling Hayden", "John Marley", "Diane Keaton"],
+    "24 March 1972",
+    "R",
+    "English",
+    "175 min",
+    ["Paramount Pictures", "Alfran Productions"],
+    ["Marlon Brando wanted to make Don Corleone \"look like a bulldog,\" so "
+     "he stuffed his cheeks with cotton wool for the audition. For the actual "
+     "filming, he wore a mouthpiece made by a dentist. This appliance is on "
+     "display in the American Museum of the Moving Image in Queens, New York.",
+     "Marlon Brando did not memorize most of his lines and read from cue cards "
+     "during most of the film.",
+     "Al Pacino's maternal grandparents emigrated to America from Corleone, "
+     "Sicily, just as Vito Corleone had.",
+     "The early buzz on the film was so positive that a sequel was planned "
+     "before the film was even finished filming.",
+     "According to Francis Ford Coppola, the film took 62 days to shoot."]
 )
