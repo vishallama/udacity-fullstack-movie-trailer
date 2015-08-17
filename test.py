609edee8ecb206fee3cd43be825bfd1b5b9eb84f
Vishal Lama <contact@vishallama.org>
2015-08-17 12:59:27 -0600

    Run tests for the Movie object godfather

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # test.py
 import unittest
 from entertainment_center \
-    import city_lights, shawshank_redemption
+    import city_lights, shawshank_redemption, godfather
 
 
 # Test: City Lights
@@ -66,6 +66,38 @@ class MovieTestShawshankRedemption(unittest.TestCase):
         self.assertEqual(
             "Morgan Freeman's favorite film of his own.",
             shawshank_redemption.trivia[0])
+
+
+# Test: The Godfather
+class MovieTestGodfather(unittest.TestCase):
+
+    def test_title(self):
+        self.assertEqual(
+            "The Godfather",godfather.title)
+
+    def test_poster_image_url(self):
+        self.assertEqual(
+            "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
+            godfather.poster_image_url)
+
+    def test_trailer_youtube_url(self):
+        self.assertEqual(
+            "https://www.youtube.com/watch?v=sY1S34973zA",
+            godfather.trailer_youtube_url)
+
+    def test_lead_actors_charles_chaplin(self):
+        self.assertEqual("Marlon Brando", godfather.lead_actors[0])
+
+    def test_lead_actors_hank_mann(self):
+        self.assertEqual("Diane Keaton", godfather.lead_actors[-1])
+
+    def test_release_date(self):
+        self.assertEqual("24 March 1972", godfather.release_date)
+
+    def test_trivia(self):
+        self.assertEqual(
+            "According to Francis Ford Coppola, the film took 62 days to shoot.",
+            godfather.trivia[-1])
 
 
 if __name__ == '__main__':
