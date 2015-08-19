1f4ff058d14a32e7e7b9a28daee54a2e8ea1eb02
Vishal Lama <contact@vishallama.org>
2015-08-19 08:59:08 -0600

    Add docstring for class Movie

--- media.py
+++ media.py
@@ -2,6 +2,7 @@
 
 
 class Movie(object):
+    """ Movie class for creating a movie """
     def __init__(self,
                  title,
                  storyline,
@@ -15,6 +16,20 @@ class Movie(object):
                  production_companies,
                  trivia
                  ):
+        """
+        Args:
+            title (str): Title of the movie.
+            storyline (str): Brief storyline of the movie.
+            poster_image_url (str): URL of the movie's poster image.
+            trailer_youtube_url (str): Youtube URL of the movie's trailer.
+            lead_actors (List[str]): A list of the main cast.
+            release_date (str): Release date of the movie.
+            mpaa_rating (str): MPAA rating of the movie.
+            language (str): Language of the movie.
+            runtime (str): Runtime of the movie.
+            production_companies (List[str]): A list of the production companies.
+            trivia (List[str]): A list of some movie trivia.
+        """
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
